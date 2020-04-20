09afbcb571c911e74f6cdcddf19d25b88b319b2f
Victor Jarvis <vctrjrvs@gmail.com>
2020-04-20 17:36:33 -0400

    added yOB function

--- greeting.js
+++ greeting.js
@@ -1,5 +1,9 @@
+function getYearOfBirth(age) {
+   return 2019 - age; 
+}
+
 function createGreeting(name, age) {
-let yearOfBirth = 2019 - age;   
+const yearOfBirth = getYearOfBirth(age);   
 return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
