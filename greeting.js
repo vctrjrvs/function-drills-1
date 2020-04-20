04abf419527b97dd1eab533bd1ff8bade528bfa8
Victor Jarvis <vctrjrvs@gmail.com>
2020-04-20 18:01:47 -0400

    added error

--- greeting.js
+++ greeting.js
@@ -1,5 +1,8 @@
 function getYearOfBirth(age) {
-   return 2019 - age; 
+    if (2019 -age < 0) {
+        throw new Error ("Age can not be negative");
+    }
+   else return 2019 - age; 
 }
 
 function createGreeting(name, age) {
@@ -7,6 +10,11 @@ const yearOfBirth = getYearOfBirth(age);
 return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
+try {
+    const greeting1 = createGreeting('Victor', -26);
+  } 
+catch(error) {
+    console.error(Error);}
 
-// let greeting1 = createGreeting();
-console.log(createGreeting('Victor', 26));
+    //let greeting1 = createGreeting();
+//console.log(createGreeting('Victor', 26));
