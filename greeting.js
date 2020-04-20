e877a1d2e987d85ce85a3db39264056f2938df6a
Victor Jarvis <vctrjrvs@gmail.com>
2020-04-20 17:11:22 -0400

    added yearOfBirth

--- greeting.js
+++ greeting.js
@@ -1,5 +1,6 @@
-function createGreeting(name, age) {   
-return `Hi, my name is ${name} and I\'m ${age} years old`;
+function createGreeting(name, age) {
+let yearOfBirth = 2019 - age;   
+return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 
