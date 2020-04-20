b6679079e8710ba23373af71a23be62b3f124a7e
Victor Jarvis <vctrjrvs@gmail.com>
2020-04-20 17:07:51 -0400

    hardcoded to take arguments

--- greeting.js
+++ greeting.js
@@ -1,6 +1,7 @@
-function createGreeting() {
-    return 'Hi, my name is Victor and I\'m 26 years old';
+function createGreeting(name, age) {   
+return `Hi, my name is ${name} and I\'m ${age} years old`;
 }
 
-let greeting1 = createGreeting();
-console.log(greeting1);
+
+// let greeting1 = createGreeting();
+console.log(createGreeting('Victor', 26));
